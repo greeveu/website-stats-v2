5bb0567a662d594bdbc58aa8071406caaa19e0b0
Kai Barzen <52884225+kaibarzen@users.noreply.github.com>
2024-03-30 18:17:38 +0100

    style(ISSUE-12): Minor coding style fixes

--- packages/stats/src/pages/player/badges/Badges.tsx
+++ packages/stats/src/pages/player/badges/Badges.tsx
@@ -6,6 +6,8 @@ import {
 	itemSchema,
 } from '../../../resources/items/items.schema.ts';
 import { Popover, Typography } from 'antd';
+import style from './badges.module.sass';
+import { observer } from 'mobx-react-lite';
 
 const pufferfishBadge: Badge = {
 	id: -1,
@@ -22,9 +24,6 @@ const axolotlBadge: Badge = {
 	item: Item.AXOLOTL_BUCKET,
 	timestamp: '2022-02-22T22:22:22Z',
 };
-
-import style from './badges.module.sass';
-import { observer } from "mobx-react-lite";
 
 interface BadgesProps {
 	result: StatsResponse;
@@ -48,61 +47,69 @@ const glowStyle: { [key in Glow]: string } = {
 	[Glow.Brown]: style.glowBrown,
 };
 
-export const Badges: React.FunctionComponent<BadgesProps> = observer((props) => {
-	const badges = useMemo(() => {
-		if (props.result.uuid === 'fba68782-d6c9-4ff9-b6dc-bdcaf50a3859') {
-			return [...props.result.stats.badges, axolotlBadge, pufferfishBadge];
-		}
-		if (props.result.uuid === '01cba8c4-ef9d-4ff9-aee3-5ba4bb38c3c8') {
-			return [...props.result.stats.badges, axolotlBadge];
-		}
-		if (props.result.uuid === 'c5588fdf-e2ef-4a50-93b1-25872b6a1a1b') {
-			return [...props.result.stats.badges, axolotlBadge];
-		}
-		if (props.result.uuid === '4aab40c1-453b-49ff-9e18-927548ccb343') {
-			return [...props.result.stats.badges, axolotlBadge];
-		}
-		return props.result.stats.badges;
-	}, [props.result.stats.badges, props.result.uuid]);
+export const Badges: React.FunctionComponent<BadgesProps> = observer(
+	(props) => {
+		const badges = useMemo(() => {
+			if (props.result.uuid === 'fba68782-d6c9-4ff9-b6dc-bdcaf50a3859') {
+				return [
+					...props.result.stats.badges,
+					axolotlBadge,
+					pufferfishBadge,
+				];
+			}
+			if (props.result.uuid === '01cba8c4-ef9d-4ff9-aee3-5ba4bb38c3c8') {
+				return [...props.result.stats.badges, axolotlBadge];
+			}
+			if (props.result.uuid === 'c5588fdf-e2ef-4a50-93b1-25872b6a1a1b') {
+				return [...props.result.stats.badges, axolotlBadge];
+			}
+			if (props.result.uuid === '4aab40c1-453b-49ff-9e18-927548ccb343') {
+				return [...props.result.stats.badges, axolotlBadge];
+			}
+			return props.result.stats.badges;
+		}, [props.result.stats.badges, props.result.uuid]);
 
-	return (
-		<div className={style.root}>
-			<div className={style.container}>
-				{badges.map((badge) => {
-					const item = getItem(badge.item);
+		return (
+			<div className={style.root}>
+				<div className={style.container}>
+					{badges.map((badge) => {
+						const item = getItem(badge.item);
 
-					const content = (
-						<div className={style.content}>
-							<Typography.Text strong={true}>
-								{badge.name}
-							</Typography.Text>
-							<Typography.Text>
-								{badge.description}
-							</Typography.Text>
-							<Typography.Text type={'secondary'}>
-								{new Date(badge.timestamp).toLocaleDateString()}
-							</Typography.Text>
-						</div>
-					);
+						const content = (
+							<div className={style.content}>
+								<Typography.Text strong={true}>
+									{badge.name}
+								</Typography.Text>
+								<Typography.Text>
+									{badge.description}
+								</Typography.Text>
+								<Typography.Text type={'secondary'}>
+									{new Date(
+										badge.timestamp,
+									).toLocaleDateString()}
+								</Typography.Text>
+							</div>
+						);
 
-					return (
-						<Popover
-							key={badge.id}
-							content={content}
-							placement={'bottom'}
-						>
-							<div
-								className={`${style.badge} ${
-									glowStyle[item.glow]
-								}`}
-								style={{
-									backgroundImage: `url(${item.image})`,
-								}}
-							/>
-						</Popover>
-					);
-				})}
+						return (
+							<Popover
+								key={badge.id}
+								content={content}
+								placement={'bottom'}
+							>
+								<div
+									className={`${style.badge} ${
+										glowStyle[item.glow]
+									}`}
+									style={{
+										backgroundImage: `url(${item.image})`,
+									}}
+								/>
+							</Popover>
+						);
+					})}
+				</div>
 			</div>
-		</div>
-	);
-});
+		);
+	},
+);
