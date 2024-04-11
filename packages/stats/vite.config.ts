929afef8e9f59bc9a6008a284d8c1f721c2044f8
Alexander <alex@pluoi.com>
2024-04-11 21:53:52 +0200

    testing

--- packages/stats/vite.config.ts
+++ packages/stats/vite.config.ts
@@ -4,6 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	base: "/stats"
+	plugins: [react(), tsconfigPaths()]
 });
